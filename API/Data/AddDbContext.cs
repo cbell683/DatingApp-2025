2503ff56e6e4bb2f17e2f6fac957eb8604cdbf0a
Clayton Bell <cbell@iss2.com>
2026-03-19 08:46:51 -0400

    End of Section 8

--- API/Data/AddDbContext.cs
+++ API/Data/AddDbContext.cs
@@ -9,5 +9,19 @@ namespace API.Data;
 public class AppDbContext(DbContextOptions options) : DbContext(options)
     {
        public DbSet<AppUser> Users { get; set; }
-    }
 
+       public DbSet<Member> Members { get; set; }
+
+       public DbSet<Photo> Photos { get; set; }
+    
+
+protected override void OnModelCreating(ModelBuilder builder)
+{    base.OnModelCreating(builder);
+
+    builder.Entity<Member>()
+        .HasOne(m => m.User)
+        .WithOne(u => u.Member)
+        .HasForeignKey<Member>(m => m.AppUserId)
+        .OnDelete(DeleteBehavior.Cascade);}
+
+}
